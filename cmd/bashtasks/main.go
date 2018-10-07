815e8b6ba7b58c3b9fc0d37494bf4611bfc32b1d
Sergey <xxsmotur@gmail.com>
2018-10-07 11:49:44 +0500

    Executing of the tasks row

--- cmd/bashtasks/main.go
+++ cmd/bashtasks/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/saromanov/bashtasks/bashtasks"
@@ -13,5 +12,5 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(cfg)
+	bashtasks.ExecuteRowTasks(cfg.Tasks)
 }
