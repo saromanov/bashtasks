3d0ce8473766d2e2abc18c6272f163e13afe68bf
Sergey <xxsmotur@gmail.com>
2018-10-17 00:07:24 +0500

    Add method getFileName

--- cmd/bashtasks/main.go
+++ cmd/bashtasks/main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/saromanov/bashtasks/bashtasks"
 )
 
+// getFileName return file name in the case
+// if this is defined as a command line argument
+// for example
+// > bashtasks foo
+// It'll load file foo.yaml from the BASHTASKS env.path
+func getFileName() string {
+	args := os.Args
+	if len(args) == 0 {
+		return ""
+	}
+	return args[1]
+}
+
 func main() {
+
 	cfg, err := bashtasks.LoadYAML("../../configs/config.yaml")
 	if err != nil {
 		log.Fatal(err)
